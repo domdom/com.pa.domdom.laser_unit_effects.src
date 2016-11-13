7033b7380b0f433a24ac6941f78b9d1e1600b6f6
domdom <domdom314@gmail.com>
2016-11-13 23:13:01 +1000

    add strategic icon generation to more pew pew

--- src/gen.py
+++ src/gen.py
@@ -9,6 +9,8 @@ from pa_tools.pa import pajson
 from pa_tools.mod.checker import check_mod
 from pa_tools.mod.generator import process_modinfo, process_changes
 
+from icons import generate_strat_icons
+
 options, warnings = pajson.loadf('conf.json')
 print ('======= Loading Options =======')
 for w in warnings:
@@ -48,24 +50,33 @@ def generate_mod(is_titans):
         print('CLASSIC')
         out_dir = '../classic'
 
-    shutil.rmtree(os.path.join(out_dir, 'pa'), ignore_errors=True)
+    # create the base file system
     src = create_source_fs(is_titans)
-
     process_modinfo('/src/pa/modinfo.json', src, out_dir)
-
     modinfo = load_json(src, '/src/pa/modinfo.json')
+
     # mount the mod directory
     src.mount('/mod', out_dir)
 
+    # remove destination files
+    shutil.rmtree(os.path.join(out_dir, 'pa'), ignore_errors=True)
+
+    ## Generate the mod
     sources = load_json(src, '/src/pa/sources.json')
     process_changes(sources, src, out_dir)
-    print ('')
 
+    # generate the stratigic icon effects
+    print ('==== running strategic icon generator')
+    src.unmount('/mod')
+    src.mount('/', out_dir)
+    process_changes(generate_strat_icons(src), src, out_dir)
+
+
+    # analyse the mod for missing files
     mod_report = check_mod(out_dir)
     print(mod_report.printReport())
 
     ################# copy mod to pa mod directory
-
     mod_path = os.path.join(paths.PA_DATA_DIR, modinfo['context'] + '_mods', modinfo['identifier'])
     shutil.rmtree(mod_path, ignore_errors=True)
     shutil.copytree(out_dir, mod_path)
